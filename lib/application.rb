b81111575fd1ae0cd20a4ac33fb4581225176c8f
Susanna <susanna.larsdotter@gmail.com>
2016-01-20 07:01:42 +0000

    ny application

--- lib/application.rb
+++ lib/application.rb
@@ -20,7 +20,7 @@ class WorkshopApp < Sinatra::Base
   DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://postgres:postgres@localhost/workshop_#{env}")
   DataMapper::Model.raise_on_save_failure = true
   DataMapper.finalize
-  DataMapper.auto_upgrade!
+  #DataMapper.auto_upgrade!
 
   before do
     @user = User.get(session[:user_id]) unless is_user?
@@ -32,7 +32,7 @@ class WorkshopApp < Sinatra::Base
         restrict_access = Proc.new { session[:flash] = 'You are not authorized to access this page'; redirect '/users/login' }
         restrict_access.call unless send("is_#{type}?")
       end
-   end
+    end
  end
 
   helpers do
