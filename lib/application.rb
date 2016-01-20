f7adb89231cfbf8fb2fa20164e5ace6d2e62e806
Susanna <susanna.larsdotter@gmail.com>
2016-01-20 07:21:02 +0000

    app

--- lib/application.rb
+++ lib/application.rb
@@ -20,7 +20,7 @@ class WorkshopApp < Sinatra::Base
   DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://postgres:postgres@localhost/workshop_#{env}")
   DataMapper::Model.raise_on_save_failure = true
   DataMapper.finalize
-  #DataMapper.auto_upgrade!
+  DataMapper.auto_upgrade!
 
   before do
     @user = User.get(session[:user_id]) unless is_user?
