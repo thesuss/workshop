28cc5746086be83a17d6f6ddaba1d18a80a8ad17
Susanna <susanna.larsdotter@gmail.com>
2016-01-31 16:57:38 +0000

    fix for missing folder on heroku

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -5,8 +5,7 @@ DatabaseCleaner.strategy = :truncation
 
 Around do |scenario, block|
   DatabaseCleaner.cleaning(&block)
-  FileUtils.rm_rf Dir['pdf/test/**/*.pdf']
-   FileUtils.rm_rf Dir['assets/img/usr/test/**/*.jpg']
+  FileUtils.rm_rf Dir['pdf/test/**/*.jpg']
 end
 
 
